e89cf9a51f2c9223703bdd8f2022e2c2606988ce
Patttron <Realisttt19@gmail.com>
2021-12-16 18:58:02 +0300

    Change formatText button

--- app/src/main/java/com/example/kotlinlearning/FormatText.kt
+++ app/src/main/java/com/example/kotlinlearning/FormatText.kt
@@ -1,5 +1,6 @@
 package com.example.kotlinlearning
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -7,6 +8,10 @@ import android.widget.EditText
 import android.widget.TextView
 
 class FormatText : AppCompatActivity() {
+    private var word: String = ""
+    private var wordMax: String = ""
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_format_text)
@@ -15,14 +20,28 @@ class FormatText : AppCompatActivity() {
         val textViewAnswer = findViewById<TextView>(R.id.textViewAnswer)
         val buttonChange = findViewById<Button>(R.id.buttonChange)
         val buttonGoBack = findViewById<Button>(R.id.buttonGoBack)
+        val textViewWordMax = findViewById<TextView>(R.id.textViewMaxWord)
 
-        buttonChange.setOnClickListener{
-            if (textEditQuestion.text.isNotEmpty()){
+        buttonChange.setOnClickListener {
+            val array = textEditQuestion.text.toString().toCharArray()
+            if (textEditQuestion.text.isNotEmpty()) {
+                for (i in array.indices) {
+                    if (array[i] != ' ') {
+                        word += array[i]
+                        if (wordMax.length < word.length) {
+                            wordMax = word
+                        }
+                    } else {
+                        word = ""
+                    }
+                }
+                textViewWordMax.text = wordMax.uppercase()
                 textViewAnswer.text = textEditQuestion.text.toString().replace('a', 'o', true)
+                textViewAnswer.text = textViewAnswer.text.toString().replace(' ', '_')
             }
         }
 
-        buttonGoBack.setOnClickListener{
+        buttonGoBack.setOnClickListener {
             finish()
         }
     }
